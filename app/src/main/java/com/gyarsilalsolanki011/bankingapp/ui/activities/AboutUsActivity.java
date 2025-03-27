0fe5c6f47cc03ab16b9e5ff1f099fb1fe668042d
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2025-03-28 04:09:51 +0530

    About Us activity designed

--- app/src/main/java/com/gyarsilalsolanki011/bankingapp/ui/activities/AboutUsActivity.java
+++ app/src/main/java/com/gyarsilalsolanki011/bankingapp/ui/activities/AboutUsActivity.java
@@ -1,26 +1,45 @@
 package com.gyarsilalsolanki011.bankingapp.ui.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.gyarsilalsolanki011.bankingapp.R;
+import com.gyarsilalsolanki011.bankingapp.databinding.ActivityAboutUsBinding;
+
+import java.util.Objects;
 
 public class AboutUsActivity extends AppCompatActivity {
+    private ActivityAboutUsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_about_us);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityAboutUsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Initialize Toolbar
+        setSupportActionBar(binding.toolbar);
+
+        // Enable Back Button
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("About Us");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) { // Handle back button
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
