b530635702954f0446382694469f6b0306934886
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2025-03-28 04:13:53 +0530

    toolbar added in Notification activity

--- app/src/main/java/com/gyarsilalsolanki011/bankingapp/ui/activities/NotificationActivity.java
+++ app/src/main/java/com/gyarsilalsolanki011/bankingapp/ui/activities/NotificationActivity.java
@@ -1,26 +1,45 @@
 package com.gyarsilalsolanki011.bankingapp.ui.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.gyarsilalsolanki011.bankingapp.R;
+import com.gyarsilalsolanki011.bankingapp.databinding.ActivityAboutUsBinding;
+import com.gyarsilalsolanki011.bankingapp.databinding.ActivityNotificationBinding;
+
+import java.util.Objects;
 
 public class NotificationActivity extends AppCompatActivity {
+    private ActivityNotificationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_notification);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityNotificationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Initialize Toolbar
+        setSupportActionBar(binding.toolbar);
+
+        // Enable Back Button
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Notifications");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) { // Handle back button
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
